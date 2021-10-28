9e94acb49ec0fd27ecbdf06287779fc5d72a927f
Ignacio Morinigo <morinacho@gmail.com>
2021-10-28 12:31:11 -0300

    Agrego el manejo de respuestas con errores identificados

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const debug   = require('debug')('app:server'); 
+const app     = express();
+const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello world!'))
 
