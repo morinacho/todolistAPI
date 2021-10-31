892591bf765f8563c2ba47b8471b730e7350cdc0
Ignacio Morinigo <morinacho@gmail.com>
2021-10-31 09:31:20 -0300

    update index pages and not found page!

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const debug    = require('debug')('app:server');
 const app      = express();
 const {Config} = require('./src/config/index');
 const {TaskAPI}   = require('./src/tasks/index');
+const {IndexAPI, NotFoundAPI} = require('./src/index/index');
 
 app.use(express.json());
 
 // Modulos
+IndexAPI(app);
 TaskAPI(app);
+NotFoundAPI(app);
 
 app.listen(Config.port, () => debug(`Servidor escuchando en el puerto ${Config.port}`))
